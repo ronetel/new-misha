5644d71dd138060be46f256fb7d437a8877a7c9e
ronetel <isip_e.a.kuznetsov@mpt.ru>
2024-05-08 16:27:08 +0300

    Добавьте файлы проекта.

---
+++ new pr 6/ViewModel/DayViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace new_pr_6.ViewModel
+{
+    internal class DayViewModel
+    {
+    }
+}
---
+++ new pr 6/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_pr_6.ViewModel
+{
+    internal class MainViewModel
+    {
+    }
+}
